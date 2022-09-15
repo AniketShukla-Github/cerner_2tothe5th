89d5f0b8fdad659d3b24a10cd1d350aeb92b7dae
AniketShukla-Github <113622633+AniketShukla-Github@users.noreply.github.com>
2022-09-15 21:33:05 +0530

    Welcome

---
+++ Day1.cpp
@@ -0,0 +1,10 @@
+//cerner_2tothe5th_2022
+#include <iostream>
+using namespace std;
+int main() {
+    
+    cout<<"************ Welcome ***********"<<endl;
+    cout<<"cerner 2^5 Annual Programming Competition   "<<endl;
+    
+    return 0;
+}
