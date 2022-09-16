2a6799c274d9d16b974624ed1ed6ea893951adc1
AniketShukla-Github <113622633+AniketShukla-Github@users.noreply.github.com>
2022-09-16 22:46:19 +0530

    Think Why???

---
+++ Day2.cpp
@@ -0,0 +1,15 @@
+//cerner_2tothe5th_2022
+//cerner_2^5_2022
+
+#include<iostream>
+ 
+using namespace std;
+int main()
+{
+    if (!(cout << "cerner"))
+    cout <<"cerner";
+    else
+    cout << " cares for everyone";
+     
+    return 0;
+}
