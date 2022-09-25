fa464c86aee5ed87e88a6e82523ac53079f362ca
AniketShukla-Github <113622633+AniketShukla-Github@users.noreply.github.com>
2022-09-25 23:02:38 +0530

    day 11

---
+++ FindTheString.exs
@@ -0,0 +1,7 @@
+#cerner_2tothe5th_2022
+#cerner_2^5_2022
+
+regex = ~r/cerner|cares/
+IO.puts("cerner is working in healthcare sector" =~ regex)
+IO.puts("it cares for the society " =~ regex)
+IO.puts("This code is being submitted as part of 2^5 coding challenge" =~ regex)
